06f5e1504658e4ab63f8d7bc7705fd3302f523b9
Mani Kumar <manikumarchintala@gmail.com>
2024-05-03 13:40:36 -0500

    functionality

--- faq-accordion-main/main.js
+++ faq-accordion-main/main.js
@@ -1,18 +1,9 @@
 document.addEventListener("DOMContentLoaded", revealcontent);
 function revealcontent() {
-  const headcontent = document.querySelectorAll(".HeaderContent");
-  const hideencontent = document.querySelectorAll(".riddle");
-  //console.log(headcontent);
+  const headcontent = document.querySelectorAll(".accord");
   headcontent.forEach((head) => {
     head.addEventListener("click", function () {
-      //const para = document.getElementsByClassName(".riddle");
-      //console.log(para);
-      //console.log(hideencontent);
-      if (head.classList.contains("riddle")) {
-        head.classList.remove("riddle");
-      } else {
-        head.classList.add("riddle");
-      }
+      this.classList.toggle("active");
     });
   });
 }
