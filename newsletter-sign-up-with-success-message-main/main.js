b61963c21a8af09cb2d792f811fc5c4efb07c890
Mani Kumar <manikumarchintala@gmail.com>
2024-05-07 16:23:29 -0500

    designs

--- newsletter-sign-up-with-success-message-main/main.js
+++ newsletter-sign-up-with-success-message-main/main.js
@@ -1,1 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const userName = document.getElementById("user");
+  let button = document.getElementById("bon");
+  //   console.log(userName.value);
+  //   console.log(button);
+  button.addEventListener("click", function () {
+    const data = userName.value;
+    console.log(data);
+  });
+});
