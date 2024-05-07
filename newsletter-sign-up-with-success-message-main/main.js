ca2db3fdf118bf6355d44be0cc100b80d3a5069a
Mani Kumar <manikumarchintala@gmail.com>
2024-05-07 17:35:24 -0500

    total functionality

--- newsletter-sign-up-with-success-message-main/main.js
+++ newsletter-sign-up-with-success-message-main/main.js
@@ -1,10 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const userName = document.getElementById("user");
-  let button = document.getElementById("bon");
-  //   console.log(userName.value);
-  //   console.log(button);
-  button.addEventListener("click", function () {
-    const data = userName.value;
-    console.log(data);
-  });
-});
+const userName = document.getElementById("user");
+let button = document.getElementById("bon");
+let button1 = document.getElementById("diss");
+let err = document.getElementById("errer");
+let id = document.getElementById("answer");
+let answer = document.getElementById("answer");
+let dismiss = document.getElementById("diss");
+let c1 = document.getElementById("container");
+let c2 = document.getElementById("container2");
+
+button.addEventListener("click", wrong);
+button1.addEventListener("click", rever);
+
+function wrong() {
+  let validRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  if (userName.value.match(validRegex)) {
+    msg.textContent = userName.value;
+    c1.style.display = "none";
+    c2.style.display = "flex";
+  } else {
+    userName.style.border = "1px solid hsl(4, 100%, 67%)";
+    userName.style.color = "hsl(4, 100%, 67%)";
+    userName.style.backgroundColor = "hsl(4, 100%, 88%)";
+    err.textContent = "Valid email required";
+  }
+}
+function rever() {
+  c1.style.display = "flex";
+  c2.style.display = "none";
+  userName.value = "";
+}
